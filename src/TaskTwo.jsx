3baf3b26120fe049aebc84efd63424c4fd71987b
ShowaybAmin <showayb72@gmail.com>
2025-02-28 20:42:20 +0600

    Just made some text color and bg color change to see if it still works.

--- src/TaskTwo.jsx
+++ src/TaskTwo.jsx
@@ -1,6 +1,6 @@
 function TaskTwo() {
   return (
-    <div className="text-xl text-green-700 text-center bg-red-800 p-2">
+    <div className="text-xl text-green-900 text-center bg-blue-200 p-2">
         We are here to learn React from tapaScript</div>
   )
 }
