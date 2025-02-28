23c9e8a23d64e3c002eac0b1c33273f82f74cb3c
ShowaybAmin <showayb72@gmail.com>
2025-02-28 20:26:47 +0600

    Started learning React and Tailwind & now uploading to github

---
+++ src/TaskTwo.jsx
@@ -0,0 +1,8 @@
+function TaskTwo() {
+  return (
+    <div className="text-xl text-green-700 text-center bg-red-800 p-2">
+        We are here to learn React from tapaScript</div>
+  )
+}
+
+export default TaskTwo
